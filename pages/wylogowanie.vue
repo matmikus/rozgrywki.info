41fa3decb367cde06c17a08c9d950de7d60202cc
Mateusz Mikołajczak <mateusz.mikolajczak@grupawp.pl>
2020-03-24 09:51:07 +0100

    poprawka adresu callbacka wylogowania

--- pages/wylogowanie.vue
+++ pages/wylogowanie.vue
@@ -13,7 +13,7 @@
             // obejście buga który loguje ostatniego użytkownika z automatu przy natępnej próbie
             // https://github.com/nuxt-community/auth-module/issues/375
             const fakeButton = document.createElement('a');
-            fakeButton.href = `https://${process.env.AUTH0_DOMAIN}/v2/logout?returnTo=http%3A%2F%2Flocalhost:3000`;
+            fakeButton.href = `https://${process.env.AUTH0_DOMAIN}/v2/logout?returnTo=${window.location.origin}`;
             fakeButton.click();
         }
     }
