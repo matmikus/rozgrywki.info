d4ae6434402025eb68fa1badc1d2127b50a53260
Mateusz Mikołajczak <mateusz.mikolajczak@grupawp.pl>
2020-03-27 17:20:07 +0100

    graphql test

--- pages/rozgrywki.vue
+++ pages/rozgrywki.vue
@@ -5,3 +5,29 @@
         </v-flex>
     </v-layout>
 </template>
+
+<script>
+    import { ApolloClient, HttpLink, InMemoryCache, gql } from "apollo-boost";
+
+    export default {
+        mounted () {
+            // GRAPHQL ENDPOINT TEST
+            const GET_COMPETITIONS = gql`query { competitions { name, id } }`;
+
+            const httpLink = new HttpLink({
+                uri: "https://rozgrywki-graphql-postgres-dev.herokuapp.com/v1/graphql"
+            });
+
+            const client = new ApolloClient({
+                link: httpLink,
+                cache: new InMemoryCache()
+            });
+
+            console.time('czas trwania');
+            client.query({
+                query: GET_COMPETITIONS
+            })
+            .then(result => {console.log(result); console.timeEnd('czas trwania');;});
+        }
+    }
+</script>
