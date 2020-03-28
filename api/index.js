453a8acd921b032fb372e4213bd920fbe74945fd
Mateusz Mikołajczak <mateusz.mikolajczak@grupawp.pl>
2020-03-28 18:02:04 +0100

    fix testu

--- api/index.js
+++ api/index.js
@@ -1,14 +1,15 @@
 const { Router } = require('express');
 const cors = require('cors');
-const router = Router();
+const express = require('express');
+const app = express();
 
-router.get('/competitions', cors(), function (req, res, next) {
+app.get('/competitions', cors(), function (req, res, next) {
     res.json({ competitions: [{ name: 'test1', id: 1 }, { name: 'test2', id: 2 }, { name: 'test3', id: 3 }] });
 });
 
-router.get('/competitions/:id', function (req, res, next) {
+app.get('/competitions/:id', function (req, res, next) {
     const id = parseInt(req.params.id)
     res.send(`rozgrywki o id ${id}`);
 });
 
-module.exports = router;
+module.exports = app;
