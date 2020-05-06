0c80faa1a59e1e250b7078f52e4828ab13ba2c09
Mateusz Mikołajczak <mateusz.mikolajczak@grupawp.pl>
2020-05-06 16:09:47 +0200

    test2

--- middleware/authorizationGraphQL.ts
+++ middleware/authorizationGraphQL.ts
@@ -1,14 +1,17 @@
 // obejście problemu odrzucanych zapytań do hasury po wylogowaniu i ustawieniu się tokena na false
 // do zapytań wykorzystujemy własne ciasteczko authToken, do którego kopiujemy token lub czyścimy
+const Cookies = require('cookies');
 
-export default function (context: { $auth: { $state: { loggedIn: boolean }, getToken: any }, res: { cookie: any, clearCookie: any } }) {
+export default function (context: { $auth: { $state: { loggedIn: boolean }, getToken: any }, res: { cookie: any, clearCookie: any }, req: any }) {
     if (process.server) {
+        const cookies = new Cookies(context.req, context.res);
         // TODO context.res nie ma metod cookie i clearCookie na heroku i sie wydupia apka
-        console.log(context.res)
         if (context.$auth.$state.loggedIn) {
-            context.res.cookie('authToken', context.$auth.getToken('auth0'));
+            // context.res.cookie('authToken', context.$auth.getToken('auth0'));
+            cookies.set('authToken', context.$auth.getToken('auth0'));
         } else {
-            context.res.clearCookie('authToken', {path:'/'});
+            // context.res.clearCookie('authToken', {path:'/'});
+            cookies.set('authToken', '', { maxAge: 0 });
         }
     }
     
