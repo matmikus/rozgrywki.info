35b5068dae68b764dde57da4d5bfbd5aff3a33b8
Mateusz Mikołajczak <mateusz.mikolajczak@grupawp.pl>
2020-07-14 18:18:25 +0200

    google auth config

---
+++ pages/logowanie.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="login-container">
+        <div>logowanie</div>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        layout: 'simple',
+        mounted () {
+            this.$auth.loginWith('google');
+        }
+    };
+</script>
+
+<style lang="scss">
+    #login-container {
+        padding-top: 56px;
+    }
+</style>
