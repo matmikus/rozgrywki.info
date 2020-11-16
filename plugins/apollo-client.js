bf79416b18095138718ef8f5dc45672115d82c84
Mateusz Mikołajczak <mateusz.mikolajczak@grupawp.pl>
2020-11-16 19:11:17 +0100

    x-hasura-uder-id fix

--- plugins/apollo-client.js
+++ plugins/apollo-client.js
@@ -3,17 +3,13 @@ import fetch from 'isomorphic-fetch';
 import { getUserId } from '@/scripts/getUserId.ts';
 
 export default function (context) {
-    const userId = getUserId(context);
-    console.log('process.env.GRAPHQL_ENDPOINT: ' + process.env.GRAPHQL_ENDPOINT);
     return {
         defaultHttpLink: false,
         link: createHttpLink({
             uri: process.env.GRAPHQL_ENDPOINT,
             credentials: 'include',
             fetch: (uri, options) => {
-                if (userId !== '') {
-                    options.headers['x-hasura-user-id'] = getUserId(context);
-                }
+                options.headers['x-hasura-user-id'] = getUserId(context);
                 
                 return fetch(uri, options);
             }
