5af041325f8cbe6c7e78d02598a6c28b1278f827
Mateusz Mikołajczak <mateusz.mikolajczak@grupawp.pl>
2020-11-16 18:43:47 +0100

    apollo client fix

--- plugins/apollo-client.js
+++ plugins/apollo-client.js
@@ -3,13 +3,17 @@ import fetch from 'isomorphic-fetch';
 import { getUserId } from '@/scripts/getUserId.ts';
 
 export default function (context) {
+    const userId = getUserId(context);
+    
     return {
         defaultHttpLink: false,
         link: createHttpLink({
             uri: process.env.GRAPHQL_ENDPOINT,
             credentials: 'include',
             fetch: (uri, options) => {
-                options.headers['x-hasura-user-id'] = getUserId(context);
+                if (userId !== '') {
+                    options.headers['x-hasura-user-id'] = getUserId(context);
+                }
                 
                 return fetch(uri, options);
             }
